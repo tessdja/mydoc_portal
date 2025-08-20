3c4ac3d3ceb5ac9b19cc0f213f2b55aea97124fd
tessdja <tessdj@gmail.com>
2025-08-20 17:03:25 -0400

    document analyzer data_analysis and data_ingestion scripts updated

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -4,4 +4,8 @@ import yaml
 def load_config(config_path: str = "config\config.yaml") -> dict:
     with open(config_path, "r") as file:
         config=yaml.safe_load(file)
+    # print(config)   # uncomment to test config loader
     return config
+
+# uncomment to test config loader
+# load_config("config\config.yaml")
--- utils/model_loader.py
+++ utils/model_loader.py
@@ -9,6 +9,7 @@ from langchain_groq import ChatGroq
 #from langchain_openai import ChatOpenAI
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
+
 log = CustomLogger().get_logger(__name__)
 
 class ModelLoader:
